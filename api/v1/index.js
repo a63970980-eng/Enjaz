cd682ac30d7ff5ee1901fc1514a0ea00857c7ab9
Salem Nasser <a63970980@gmail.com>
2026-09-05 21:56:56 +0300

    fix(api): add explicit versioned API function route

---
+++ api/v1/index.js
@@ -0,0 +1,7 @@
+import apiModule from '../../api/server.cjs';
+
+const server = apiModule.server || apiModule.default?.server || apiModule.default || apiModule;
+
+export default function handler(req, res) {
+  return server.emit('request', req, res);
+}
