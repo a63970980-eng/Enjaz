e5a56edcb26bb44a28c581bde479cfe106b6364f
Salem Nasser <a63970980@gmail.com>
2026-09-05 21:58:19 +0300

    fix(api): support deep versioned routes through Vercel rewrite

--- api/index.js
+++ api/index.js
@@ -3,5 +3,11 @@ import apiModule from './server.cjs';
 const server = apiModule.server || apiModule.default?.server || apiModule.default || apiModule;
 
 export default function handler(req, res) {
+  const incoming = new URL(req.url || '/', `http://${req.headers.host || 'localhost'}`);
+  const forwardedPath = incoming.searchParams.get('__enjaz_path');
+  if (forwardedPath) {
+    incoming.searchParams.delete('__enjaz_path');
+    req.url = `${forwardedPath}${incoming.search ? `?${incoming.searchParams.toString()}` : ''}`;
+  }
   return server.emit('request', req, res);
 }
