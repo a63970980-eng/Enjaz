202fe1f31d24c894369ad968b9fc40ec78925704
Salem Nasser <a63970980@gmail.com>
2026-09-05 21:57:11 +0300

    fix(api): explicitly route versioned API subpaths

---
+++ api/v1/[...path].js
@@ -0,0 +1,7 @@
+import apiModule from '../server.cjs';
+
+const server = apiModule.server || apiModule.default?.server || apiModule.default || apiModule;
+
+export default function handler(req, res) {
+  return server.emit('request', req, res);
+}
