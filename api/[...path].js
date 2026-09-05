b6e0d348554682433dfd400e7ac2b6e6bfec4f41
Salem Nasser <a63970980@gmail.com>
2026-09-05 21:49:28 +0300

    fix(api): use native ESM Vercel handler

--- api/[...path].js
+++ api/[...path].js
@@ -1,14 +1,6 @@
-let serverPromise;
+process.env.ENJAZ_VERCEL = '1';
+import { server } from '../services/api/src/index.js';
 
-async function getServer() {
-  if (!serverPromise) {
-    process.env.ENJAZ_VERCEL = '1';
-    serverPromise = import('../services/api/src/index.js').then(({ server }) => server);
-  }
-  return serverPromise;
-}
-
-module.exports = async function handler(req, res) {
-  const server = await getServer();
+export default function handler(req, res) {
   return server.emit('request', req, res);
-};
+}
