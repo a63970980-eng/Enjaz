b1d367229c73677dfe5e11003c5ddcb7572bd047
Salem Nasser <a63970980@gmail.com>
2026-09-23 14:43:48 +0300

    chore(web): remove legacy public interface mount

--- apps/web/main.js
+++ apps/web/main.js
@@ -1,7 +1,6 @@
 import './boot-config.js';
 import './enjaz-enterprise-finish.css';
 import './enjaz-reference-v4.css';
-import './enjaz-public.js';
 
 const authRoute=new URLSearchParams(window.location.search).has('auth');
 
